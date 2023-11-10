4883e8814993339c411086b82367ecc6ae5ecdd7
suraj panigrahi <surajpanigrahi296@gmail.com>
2023-11-10 12:07:28 +0530

    model file updated

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -42,6 +42,13 @@ class DataIngestion:
             test_data.to_csv(self.ingestion_config.test_data_path,index=False)
             
             logging.info("data ingestion part completed")
+
+            return (
+                 
+                
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
             
             
         except Exception as e:
